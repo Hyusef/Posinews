2f41ea4ec4e7ac4646904a42d0a905c3cb0ecf0a
therealestdeal <mieyle@protonmail.com>
2022-03-16 07:54:47 +0000

    fetch data from backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import styled from 'styled-components';
 import Navbar from "./components/Navbar"
+import axios from "axios";
+import {useState,useEffect} from "react"
 
 function App() {
+  const [newsInfo,setNewsInfo] = useState([1,2,3]);
+
+  useEffect(()=>{
+    axios.get("/good").then(res=>{
+      setNewsInfo(res.data)
+      }).catch(err=>console.log(err));
+  },[])
+
+
   return (
     <div className="App"> 
     <Navbar/>
-    </div>
+      {newsInfo.map((ele,i)=>{
+    return (
+    <>
+    <img width="400px" src={`https://${ele.newsImage}`}></img>
+      <p>{ele.newsTitle}</p>
+      </>
+    
+    )
+      })}
+  
+</div>
   );
 }
 
