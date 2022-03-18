47de942cfae60f44b57f5fd890c93d128d739e1e
therealestdeal <mieyle@protonmail.com>
2022-03-18 07:18:50 +0000

    Add styled compnent for card with links

--- src/components/Newscard.jsx
+++ src/components/Newscard.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
-function Newscard({image,title,link}) {
-    return (
-        <div>
-            
-        </div>
-    )
+const CardBox = styled.div`
+  display: flex;
+  border: 1px solid black;
+  margin: 5px;
+  width: 60%;
+  padding: 5px;
+  align-items:center;
+`;
+
+function Newscard({ image, title, link }) {
+  return (
+    <div>
+      <CardBox>
+        <a href={link}>
+          <img src={`https://${image}`}></img>
+        </a>
+        <a href={link}>
+          <p>{title}</p>
+        </a>
+      </CardBox>
+    </div>
+  );
 }
 
-export default Newscard
+export default Newscard;
