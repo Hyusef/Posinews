d7ad6ee6780cecfd53b763752eaa5309a2ae221d
therealestdeal <mieyle@protonmail.com>
2022-03-19 07:53:45 +0000

    Add styling for innerCardBox

--- src/components/Newscard.jsx
+++ src/components/Newscard.jsx
@@ -10,12 +10,24 @@ const CardBox = styled.div`
   :hover {
     box-shadow: 1px 1px #373737, 2px 2px #373737, 3px 3px #373737,
       4px 4px #373737, 5px 5px #373737, 6px 6px #373737;
+      background:#fcfcfc;
     -webkit-transform: translateX(-3px);
     transform: translateX(-3px);
     transition: 0.5s ease;
   }
   cursor: pointer;
 `;
+
+const InnerCardBox = styled.div`
+display:flex;
+align-items:center;
+font-size:20px;
+margin:2px;
+p{
+  margin-left:7px;  
+}
+`;
+
 
 
 
@@ -26,14 +38,14 @@ function Newscard({ image, title, link }) {
     <div>
       <a href={link}>
         <CardBox>
-          <div style={{ display: "flex" }}>
-            <img width="140px" src={ image?`https://${image}`:"https://images.pexels.com/photos/4185957/pexels-photo-4185957.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}></img>
+          <InnerCardBox>
+            <img width="140px" alt="News Thumbnails" src={ image?`https://${image}`:"https://images.pexels.com/photos/4185957/pexels-photo-4185957.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}></img>
             <p>{title}</p>
-          </div>
+          </InnerCardBox>
         </CardBox>
       </a>
     </div>
-  );
+  );  
 }
 
 export default Newscard;
