f88b022971b9fa85f07808c4ecc516dd19cb2226
therealestdeal <mieyle@protonmail.com>
2022-03-19 07:57:28 +0000

    format newscard

--- src/components/Newscard.jsx
+++ src/components/Newscard.jsx
@@ -10,7 +10,7 @@ const CardBox = styled.div`
   :hover {
     box-shadow: 1px 1px #373737, 2px 2px #373737, 3px 3px #373737,
       4px 4px #373737, 5px 5px #373737, 6px 6px #373737;
-      background:#fcfcfc;
+    background: #fcfcfc;
     -webkit-transform: translateX(-3px);
     transform: translateX(-3px);
     transition: 0.5s ease;
@@ -19,19 +19,14 @@ const CardBox = styled.div`
 `;
 
 const InnerCardBox = styled.div`
-display:flex;
-align-items:center;
-font-size:20px;
-margin:2px;
-p{
-  margin-left:7px;  
-}
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+  margin: 2px;
+  p {
+    margin-left: 7px;
+  }
 `;
-
-
-
-
-
 
 function Newscard({ image, title, link }) {
   return (
@@ -39,13 +34,21 @@ function Newscard({ image, title, link }) {
       <a href={link}>
         <CardBox>
           <InnerCardBox>
-            <img width="140px" alt="News Thumbnails" src={ image?`https://${image}`:"https://images.pexels.com/photos/4185957/pexels-photo-4185957.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}></img>
+            <img
+              width="140px"
+              alt="News Thumbnails"
+              src={
+                image
+                  ? `https://${image}`
+                  : "https://images.pexels.com/photos/4185957/pexels-photo-4185957.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              }
+            ></img>
             <p>{title}</p>
           </InnerCardBox>
         </CardBox>
       </a>
     </div>
-  );  
+  );
 }
 
 export default Newscard;
