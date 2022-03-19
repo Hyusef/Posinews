d5296e21e459d00a747718029f66e0707d60673f
therealestdeal <mieyle@protonmail.com>
2022-03-19 06:37:06 +0000

    Make entire div into link

--- src/components/Newscard.jsx
+++ src/components/Newscard.jsx
@@ -2,38 +2,36 @@ import React from "react";
 import styled from "styled-components";
 
 const CardBox = styled.div`
-  display: flex;
+  display: inline-block;
   border: 1px solid black;
-  margin: 5px;
+  margin: 10px;
   width: 60%;
-  padding: 5px;
-  align-items:center;
-  :hover{
-    box-shadow:
-    1px 1px #373737,
-    2px 2px #373737,
-    3px 3px #373737,
-    4px 4px #373737,
-    5px 5px #373737,
-    6px 6px #373737;
+  padding: 15px;
+  :hover {
+    box-shadow: 1px 1px #373737, 2px 2px #373737, 3px 3px #373737,
+      4px 4px #373737, 5px 5px #373737, 6px 6px #373737;
     -webkit-transform: translateX(-3px);
     transform: translateX(-3px);
-    transition: .5s ease;
-    }
+    transition: 0.5s ease;
+  }
+  cursor: pointer;
+`;
+
+const Link = styled.a`
 
 `;
 
 function Newscard({ image, title, link }) {
   return (
     <div>
-      <CardBox>
-        <a href={link}>
-          <img src={`https://${image}`}></img>
-        </a>
-        <a href={link}>
-          <p>{title}</p>
-        </a>
-      </CardBox>
+      <Link href={link}>
+        <CardBox>
+          <div style={{ display: "flex" }}>
+            <img src={`https://${image}`}></img>
+            <p>{title}</p>
+          </div>
+        </CardBox>
+      </Link>
     </div>
   );
 }
