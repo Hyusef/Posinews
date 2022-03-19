5565ccc34279d62a0477802a371c64ea8ebc608a
therealestdeal <mieyle@protonmail.com>
2022-03-19 08:08:33 +0000

    Add styled header component with text

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import styled from "styled-components"
 
+const Header = styled.h1`
+font-size:3.2rem;
+font-family:Verdana;
+`
 function Headline() {
     return (
         <div>
-            
+            <Header>Today's Headlines</Header>
         </div>
     )
 }
-
 export default Headline
