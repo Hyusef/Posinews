9bc59d2a82f0eff51db00575050cad2ee1c36c93
therealestdeal <mieyle@protonmail.com>
2022-03-19 07:56:26 +0000

    Add file for headline

---
+++ src/components/Headline.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Headline() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Headline
