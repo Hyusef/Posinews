aa49d1bcb6af4916d74eb7ec9ac3badfbfc4cd54
therealestdeal <mieyle@protonmail.com>
2022-03-19 06:34:05 +0000

    Format file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import './App.css';
-import styled from 'styled-components';
-import Navbar from "./components/Navbar"
+import "./App.css";
+import styled from "styled-components";
+import Navbar from "./components/Navbar";
 import axios from "axios";
-import {useState,useEffect} from "react"
-import Newscard from './components/Newscard';
+import { useState, useEffect } from "react";
+import Newscard from "./components/Newscard";
 
 function App() {
-  const [newsInfo,setNewsInfo] = useState([1,2,3]);
+  const [newsInfo, setNewsInfo] = useState([1, 2, 3]);
 
-  useEffect(()=>{
-    axios.get("/good").then(res=>{
-      setNewsInfo(res.data)
-      }).catch(err=>console.log(err));
-  },[])
+  useEffect(() => {
+    axios
+      .get("/good")
+      .then((res) => {
+        setNewsInfo(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
-    <div className="App"> 
-    <Navbar/>
-      {newsInfo.map((ele,i)=>{
-    return (
-    <Newscard image={ele.newsImage} title={ele.newsTitle} link={ele.newsUrl}/>
-    )
+    <div className="App">
+      <Navbar />
+      {newsInfo.map((ele, i) => {
+        return (
+          <Newscard
+            image={ele.newsImage}
+            title={ele.newsTitle}
+            link={ele.newsUrl}
+          />
+        );
       })}
-  
-</div>
+    </div>
   );
 }
+
 
 export default App;
